70a4acc31c175690e467473df615a2c79adb9cbd
Sakshi08429 <chaudharysakshi5362@gmail.com>
2025-04-20 22:43:17 +0530

    Layout

---
+++ client/providers/SlideBarProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Sidebar from "@/app/Components/SlideBar/SlideBar";
+import { useUserContext } from "@/context/userContext";
+import React from "react";
+
+function SidebarProvider() {
+  const userId = useUserContext().user._id;
+  return <>{userId && <Sidebar />}</>;
+}
+
+export default SidebarProvider;
